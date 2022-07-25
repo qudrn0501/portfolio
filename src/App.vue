a337e0986c20246e05262614bbecf8ea6ad0a076
qudrn0501 <qudrn0501@naver.com>
2022-07-25 17:48:15 +0900

    0725 start

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,111 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="top"></div>
+  <header>
+    <div class="logo">bkKim.</div>
+    <div class="btn_nav">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  
+    <nav class="mapsite"> <!--mapsite menu-->
+      <div class="btn_close"></div>
+      <ul>
+        <li v-on:click="gotointro">intro</li>
+        <li v-on:click="gotoskill">skills</li>
+        <li v-on:click="gotoproject">project</li>
+        <li v-on:click="gotowebclone">webclone</li>
+        <li v-on:click="gotocontact">contact</li>
+      </ul>
+    </nav>
+  </header>
+  <intro id="intro"></intro>
+  <skill id="skill"></skill>
+  <project id="project"></project>
+  <webclone id="webclone"></webclone>
+  <contact id="contact"></contact>
+  <div class="topbtn" v-on:click="gototop">TOP</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import intro from './components/intro.vue'
+import skill from './components/skill.vue'
+import project from './components/project.vue'
+import webclone from './components/webclone.vue'
+import contact from './components/contact.vue'
 
 export default {
   name: 'App',
+  methods: {
+    gotointro() {
+      const contact = document.getElementById('intro')
+      if(contact) {
+        contact.scrollIntoView({behavior : 'smooth'})
+      }
+    },
+    gotoskill() {
+      const contact = document.getElementById('skill')
+      if(contact) {
+        contact.scrollIntoView({behavior : 'smooth'})
+      }
+    },
+    gotoproject() {
+      const contact = document.getElementById('project')
+      if(contact) {
+        contact.scrollIntoView({behavior : 'smooth'})
+      }
+    },
+    gotowebclone() {
+      const contact = document.getElementById('webclone')
+      if(contact) {
+        contact.scrollIntoView({behavior : 'smooth'})
+      }
+    },
+    gotocontact() {
+      const contact = document.getElementById('contact')
+      if(contact) {
+        contact.scrollIntoView({behavior : 'smooth'})
+      }
+    },
+    gototop() {
+      const contact = document.getElementById('top')
+      if(contact) {
+        contact.scrollIntoView({behavior : 'smooth'})
+      }
+    }
+  },
   components: {
-    HelloWorld
+    intro,
+    skill,
+    project,
+    webclone,
+    contact
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+#top {
+  display: none;
+}
+.mapsite {
+  display: none;
+}
+header {
+  width: 100%; height: 80px;
+  background-color: aqua;
+  position: fixed;
+}
+header .logo {
+  font-family: "P22 Underground";
+  font-size: 40px;
+  font-weight: 600;
+  color: #3182F6;
+}
+
+.topbtn {
+  position: fixed;
+  bottom: 30px;
+  right: 30px;
 }
 </style>
---
+++ src/components/project.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="hello">
+ <h1>project</h1>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'project-page',
+  components: {
+
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  padding-bottom: 1080px;
+}
+</style>
